5a00b0988cad21d1704a7b5201866dacf1fe1816
KimDaeil <daelous@naver.com>
2018-04-17 00:16:47 +0900

    define custom class for data type wrapped SQLAlchemy.Column   - String   - Int   - BigInt   - DateTime

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,9 +2,46 @@
 
 from . import *
 from core.server.utils.orm import db
+import datetime
 
-__all__ = []
+__all__ = ["DBModel", "String", "Int", "DateTime", "BigInt"]
+
+DBModel = db.Model
 
 
-class BaseOrmModel(db.Model):
-    pass
+class BaseColumn(db.Column):
+    def __init__(self, *args, **kwarg):
+        if 'nullable' not in kwarg:
+            kwarg['nullable'] = False
+
+        super().__init__(*args, **kwarg)
+
+
+class String(BaseColumn):
+    def __init__(self, name, length=0, **kwargs):
+        if length == 0:
+            length = None
+
+        super().__init__(name, db.String(length), **kwargs)
+
+
+class Int(BaseColumn):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, db.Integer, **kwargs)
+
+
+class BigInt(BaseColumn):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, db.BigInteger, **kwargs)
+
+
+class DateTime(BaseColumn):
+    def __init__(self, name, **kwargs):
+        # TODD 2018. 04. 17. make sure that is timezone
+        # consider below terms
+        # 1. making time zone about server and user
+        # 2. make time zone about server
+        if 'default' not in kwargs:
+            kwargs['default'] = datetime.datetime.utcnow()
+
+        super().__init__(name, db.DateTime, **kwargs)
