c243eb01a028a8ccba7ff3447545fe9f9ab42848
KimDaeil <daelous@naver.com>
2018-05-14 14:52:39 +0900

    update test infomation for mongodb

--- core/config.py
+++ core/config.py
@@ -6,14 +6,14 @@ class Config(object):
 
 class TestConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://root:root1592@api-dev-rds.c2w11tiph4ya.ap-northeast-2.rds.amazonaws.com:5432/test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://root:12345678@db-dev.c2w11tiph4ya.ap-northeast-2.rds.amazonaws.com:5432/test'
     SECRET_KEY = "LALAVLA"
 
     MONGODB_DBNAME = 'test'
-    MONGODB_HOST = 'ec2-13-125-217-177.ap-northeast-2.compute.amazonaws.com'
+    MONGODB_HOST = 'ec2-13-125-241-234.ap-northeast-2.compute.amazonaws.com'
     MONGODB_PORT = 27017
     MONGODB_USERNAME = 'test'
-    MONGODB_PASSWORD = 'test27017'
+    MONGODB_PASSWORD = '12345678'
 
 
 class ProductConfig(Config):
